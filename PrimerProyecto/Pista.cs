d22c0d4d5a82e50dae337fd0ad512ef21e4c0f29
Jaime <9408714@alumnos.ufv.es>
2025-04-10 19:28:36 +0200

    primer commit

---
+++ PrimerProyecto/Pista.cs
@@ -0,0 +1,18 @@
+public class Pista{
+    private string? ID;
+    public string GetId(){
+        return ID;
+    }
+    public void SetId(string iD){
+        iD = ID;
+    }
+    private enum Estado{
+        Libre,
+        Ocupada
+    }
+    public void AvionActual(Avion avion){
+
+    }
+
+    piblic int DisponibilidadTicks(int )
+}
