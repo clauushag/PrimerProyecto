7d68e7cbc7d020c065c6cdbf7506f63c8752d3ac
Jaime <9408714@alumnos.ufv.es>
2025-04-17 12:21:27 +0200

    clase pista hecha y avion de carga duplicado borrado

--- PrimerProyecto/AvionDeCarga.cs
+++
@@ -1,8 +0,0 @@
-public class AvionDeCarga : Avion
-{
-    public double CargaMaxima { get; set; }
-    public AvionDeCarga(double cargMax, string id, int dis, int vel, double capComb, double consComb, double combAct) : base(id, dis, vel, capComb, consComb, combAct)
-    {
-        CargaMaxima = cargMax;
-    }
-}
